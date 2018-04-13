3760bc720b36e614a80733fd39ea8b64dfbac32e
HatemMS2012 <h.m.sergieh@gmail.com>
2018-04-13 10:39:48 +0200

    Update sample_import.py

--- sample_import.py
+++ sample_import.py
@@ -19,7 +19,7 @@ def insert_dataframe_into_db(df,table_name,db_url,db_driver):
                   properties={"driver": db_driver})
 
 
-df_json = spark.read.json("sample_data/student.json", multiLine=True)
+df_json = spark.read.json("/home/hmoussel/myscript/student.json", multiLine=True)
 
 df_json.printSchema()
 
