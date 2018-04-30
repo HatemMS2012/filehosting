f3bbe56af022f81f86f9de58248353b5227a3486
HatemMS2012 <h.m.sergieh@gmail.com>
2018-04-30 11:25:16 +0200

    Update sample_import.py

--- sample_import.py
+++ sample_import.py
@@ -8,7 +8,7 @@ spark = SparkSession.builder.appName("Python Spark SQL basic example").\
     .config("spark.master","local[*]").getOrCreate()
 
 
-db_url = "jdbc:mysql://mysql.marathon.l4lb.thisdcos.directory:3306/student?user=root&password=root"
+db_url = "jdbc:mysql://00.66.1.15:5901/student?user=root&password=root"
 db_driver = "com.mysql.jdbc.Driver"
 table_name = "student"
 
