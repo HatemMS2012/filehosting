41918a0ca441a66f8c6fa68a7d24617020ce184b
HatemMS2012 <h.m.sergieh@gmail.com>
2018-04-30 16:12:12 +0200

    Update sample_import.py

--- sample_import.py
+++ sample_import.py
@@ -1,18 +1,21 @@
 from pyspark.sql import SparkSession
 from pyspark.sql import Row
 
-print("hello")
+print("hello 2")
 
 
 #https://github.com/HatemMS2012/filehosting/blob/master/mysql-connector-java-5.1.38.jar
 spark = SparkSession.builder.appName("Python Spark SQL basic example").\
     config('spark.driver.extraClassPath','http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.38/mysql-connector-java-5.1.38.jar')\
-    .config("spark.master","local[*]").getOrCreate()
+    config("spark.jars", 'http://central.maven.org/maven2/mysql/mysql-connector-java/5.1.38/mysql-connector-java-5.1.38.jar').config("spark.master","local[*]").getOrCreate()
 
 
 db_url = "jdbc:mysql://100.66.1.15:5901/student?user=root&password=root"
 db_driver = "com.mysql.jdbc.Driver"
 table_name = "student"
+
+
+
 
 def insert_dataframe_into_db(df,table_name,db_url,db_driver):
     df.write.jdbc(url=db_url,
@@ -21,6 +24,30 @@ def insert_dataframe_into_db(df,table_name,db_url,db_driver):
                   properties={"driver": db_driver})
 
 
+print("mysql config2 start ")
+sql = SQLContext(sc)
+df = sql.read.format('jdbc').options(
+   url='jdbc:mysql://100.66.1.15:5901/student',
+   user='root',
+   password='root',
+   driver="com.mysql.jdbc.Driver",
+   dbtable='student'
+).load() 
+ 
+print("mysql config 2")
+
+
+print("mysql config  3 start ")
+sql = SQLContext(sc)
+df = sql.read.format('jdbc').options(
+   url='jdbc:mysql://100.66.1.15:5901/student',
+   user='root',
+   password='root',
+   driver="org.mariadb.jdbc.Driver",
+   dbtable='student'
+).load() 
+ 
+print("mysql config 3")
 #df_json = spark.read.json("file://student.json", multiLine=True)
 # Create a data frame manually
 
